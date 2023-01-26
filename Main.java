762ee1dd72414c00beb94e0ccb80e9e8a2624f3d
Ryan Henzell-Hill <95857127+Ryhhill1998@users.noreply.github.com>
2023-01-26 13:40:16 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {3, -2, -1, 3, 2, 5, -1, 2};
+        System.out.println(solution(array));
+    }
+
+    private static int solution(int[] A) {
+        int maxSliceSum = A[0];
+        int runningTotal = Math.max(maxSliceSum, 0);
+
+        for (int i = 1; i < A.length; i++) {
+            int current = A[i];
+
+            if (current > runningTotal
+                    && current + runningTotal < runningTotal) {
+                runningTotal = current;
+            } else {
+                runningTotal += current;
+            }
+
+            maxSliceSum = Math.max(maxSliceSum, runningTotal);
+        }
+
+        return maxSliceSum;
+    }
+}
