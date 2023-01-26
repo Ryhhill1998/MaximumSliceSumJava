419f9b40a839a20e0af6acff8953cd99df532038
Ryan Henzell-Hill <95857127+Ryhhill1998@users.noreply.github.com>
2023-01-26 13:46:04 +0000

    Add files via upload

--- Main.java
+++ Main.java
@@ -11,8 +11,7 @@ public class Main {
         for (int i = 1; i < A.length; i++) {
             int current = A[i];
 
-            if (current > runningTotal
-                    && current + runningTotal < runningTotal) {
+            if (current > current + runningTotal) {
                 runningTotal = current;
             } else {
                 runningTotal += current;
